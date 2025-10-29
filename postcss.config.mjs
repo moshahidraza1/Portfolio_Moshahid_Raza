d83821da89c85cd626e432177d924f68ca90b5b3
moshahidraza1 <codinghandle@gmail.com>
2025-10-29 14:18:38 +0530

    added: Navbar

---
+++ postcss.config.mjs
@@ -0,0 +1,20 @@
+const config = {
+  theme:{
+    extend:{
+      colors:{
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F'
+      },
+      fontFamily:{
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      }
+    }
+  },
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+export default config;
